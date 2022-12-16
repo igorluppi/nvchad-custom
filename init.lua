09ed059b596fd4e9826d9d31d991d4d1d4b00df9
Igor Luppi <igor.luppi@gmail.com>
2022-12-16 00:11:03 -0300

    new keys

--- init.lua
+++ init.lua
@@ -1,7 +1,22 @@
 local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+local g = vim.g
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+
+-- Options
+opt.timeoutlen = 900
+
+-- Neovide Config
+vim.api.nvim_exec([[
+if exists("g:neovide")
+    " Put anything you want to happen only in Neovide here
+    let g:neovide_transparency = 0.95
+    " let g:neovide_scroll_animation_length = 0.3
+endif
+]], true)
--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,11 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "command mode", opts = { nowait = true } },
+    -- save
+    ["<C-a>"] = { "<cmd> wa <CR>", "save all files" },
+    ["<leader>q"] = { "<cmd> qa <CR>", "quit" },
+    ["<leader>ot"] = { "<cmd> term <CR>i", "open terminal" },
+
   },
 }
 
